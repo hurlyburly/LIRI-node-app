9018e07ccfb2bd433d7f1b4bf192fda206a45028
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-05-18 23:19:38 -0500

    set up with API NPM packages and keys, etc.

---
+++ liri.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var nodeArg=process.argv[2];
+
+switch (nodeArg) {
+    case "my-tweets":
+
+      myTweets();
+      break;
+    case "spotify-this-song":
+      spotifyThisSong();
+      break;
+    case "movie-this":
+      movieThis();
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+  }
+var myTweets=function(){
+    console.log(client);
+}
