406dbba618c7196ecda5691a2147e03bef801288
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-05-20 13:35:00 -0500

    refactoring functions related to the spotify API. Adding in clarifying comments

--- liri.js
+++ liri.js
@@ -1,59 +1,62 @@
+//linking all the necessary packages in order to run the program
 require("dotenv").config();
 var keys = require("./keys");
 var request = require("request");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
-
 var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var nodeArg = process.argv[2];
 
+//List of functions for each node commang in process.argv[2] that grab the necessary info from the Twitter, Spotify and OMDB APIs passing callbacks to the print functions related to each get function
 var getMyTweets = function() {
   var params = { screen_name: client.consumer_key };
-  client.get("statuses/user_timeline", params, function(
-    error,
-    tweets,
-    response
-  ) {
-    print20Tweets(error, tweets, response);
-  });
+  client.get("statuses/user_timeline", params, printTweets);
 };
-var spotifyThisSong = function() {
+
+var getSpotifySong = function() {
+  //setting search param : title to a single string by taking each argument after nodeArg and joinng them 
   var title = process.argv.splice(3).join(" ");
-  spotify.search({ type: 'track', query:title}, function(err, data) {
-    var song=data.tracks.items[0].album;
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-    console.log("\n"+song.artists[0].name+"\n"+song.preview_url+"\n"+song.name+"\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
-
-  })
+  spotify.search({ type: "track", query: title }, printSpotifySong);
 };
 
-var movieThis = function() {};
-var doWhatItSays = function() {};
+var getMovie = function() {
 
-switch (nodeArg) {
-  case "my-tweets":
-    getMyTweets();
-    break;
-  case "spotify-this-song":
-    spotifyThisSong();
-    break;
-  case "movie-this":
-    movieThis();
-    break;
-  case "do-what-it-says":
-    doWhatItSays();
-    break;
+};
+var getRandomCommand = function() {
+
+};
+
+//List of Functions passed as callbacks in the get functions for each node command in process.argv[2]. These will print the necessary information to the console as well as call the appendLog function once information is received. 
+
+function printSpotifySong(err, data) {
+  var song = data.tracks.items[0];
+  if (err) {
+    return console.log("Error occurred: " + err);
+  }
+  var songInfo =
+    "\n" +
+    song.album.artists[0].name +
+    "\n" +
+    song.preview_url +
+    "\n" +
+    song.album.name +
+    "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n";
+  console.log(songInfo);
+  appendLog(songInfo);
 }
 
-function print20Tweets(error, tweets,response) {
+function printTweets(error, tweets, response, tweetLimit) {
+  if (error) {
+    return console.log("Error occurred: " + err);
+  }
   if (!error) {
-    for (i = 0; i < 20; i++) {
-      var printTweets = "\n" +
+    tweetLimit = 20;
+    for (i = 0; i < tweetLimit; i++) {
+      var printTweets =
+        "\n" +
         tweets[i].text +
         "\n" +
         tweets[i].created_at +
@@ -64,10 +67,26 @@ function print20Tweets(error, tweets,response) {
   }
 }
 
+
+// This will append the information pulled from the API call to the log.txt file
 function appendLog(print) {
-  fs.appendFile("log.txt", print, function (err) {
-    if (err)
-      throw err;
+  fs.appendFile("log.txt", print, function(err) {
+    if (err) throw err;
   });
 }
 
+//This is the switch case that will run a function depending on which argument the user passes as nodeArg(process.argv[2])
+switch (nodeArg) {
+  case "my-tweets":
+    getMyTweets();
+    break;
+  case "spotify-this-song":
+    getSpotifySong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+}
