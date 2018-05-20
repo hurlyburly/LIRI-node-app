a52e18e5c1a29c6ba4af58b4d9e5ba30794f1bc3
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-05-20 00:15:01 -0500

    Cleaned up myTweets function, refactored out function print20Tweets() and appendLog()

--- liri.js
+++ liri.js
@@ -1,45 +1,62 @@
 require("dotenv").config();
-var keys=require("./keys");
-var Twitter=require("twitter");
-var Spotify=require("node-spotify-api");
+var keys = require("./keys");
+var request = require("request");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
 var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-var nodeArg=process.argv[2];
+var nodeArg = process.argv[2];
 
-var myTweets=function(){
-    var params = {screen_name: 'liri_kb'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
-        for(i=0;i<20;i++){
-        console.log("\n"+tweets[i].text+"\n"+tweets[i].created_at+"\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
-      }
-    }
-    });
-}
-var spotifyThisSong=function(){
-
-}
-var movieThis=function(){
-
-}
-var doWhatItSays=function(){
-
-}
+var myTweets = function() {
+  var params = { screen_name: client.consumer_key };
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    print20Tweets(error, tweets, response);
+  });
+};
+var spotifyThisSong = function() {};
+var movieThis = function() {};
+var doWhatItSays = function() {};
 
 switch (nodeArg) {
-    case "my-tweets":
-      myTweets();
-      break;
-    case "spotify-this-song":
-      spotifyThisSong();
-      break;
-    case "movie-this":
-      movieThis();
-      break;
-    case "do-what-it-says":
-      doWhatItSays();
-      break;
+  case "my-tweets":
+    myTweets();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+}
+
+function print20Tweets(error, tweets,response) {
+  if (!error) {
+    for (i = 0; i < 20; i++) {
+      var printTweets = "\n" +
+        tweets[i].text +
+        "\n" +
+        tweets[i].created_at +
+        "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n";
+      console.log(printTweets);
+      appendLog(printTweets);
+    }
   }
+}
+
+function appendLog(print) {
+  fs.appendFile("log.txt", print, function (err) {
+    if (err)
+      throw err;
+  });
+}
+
