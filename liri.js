18f0f1669eaec1c42213bbd05347a1b8f635551c
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-05-19 23:52:24 -0500

    Added in my-tweet functionality

--- liri.js
+++ liri.js
@@ -1,11 +1,33 @@
 require("dotenv").config();
-require("twitter").config();
-require("spotify").config();
+var keys=require("./keys");
+var Twitter=require("twitter");
+var Spotify=require("node-spotify-api");
+
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
 var nodeArg=process.argv[2];
+
+var myTweets=function(){
+    var params = {screen_name: 'liri_kb'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        for(i=0;i<20;i++){
+        console.log("\n"+tweets[i].text+"\n"+tweets[i].created_at+"\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
+      }
+    }
+    });
+}
+var spotifyThisSong=function(){
+
+}
+var movieThis=function(){
+
+}
+var doWhatItSays=function(){
+
+}
 
 switch (nodeArg) {
     case "my-tweets":
@@ -21,6 +43,3 @@ switch (nodeArg) {
       doWhatItSays();
       break;
   }
-var myTweets=function(){
-    console.log(client);
-}
