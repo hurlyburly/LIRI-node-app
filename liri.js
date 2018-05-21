07f550b50f34d42b85de51ec2ae604a46ebf7749
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-05-20 23:15:57 -0500

    Added in code for 'do-what-it-says' argument

--- liri.js
+++ liri.js
@@ -9,16 +9,21 @@ var fs = require("fs");
 //Initializing Spotify and Twitter
 var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
+
+//Setting variables for Node arguments
 var nodeArg = process.argv[2];
-var title = process.argv.splice(3).join(" ").replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-//List of functions for each node commang in process.argv[2] that grab the necessary info from the Twitter, Spotify and OMDB APIs passing callbacks to the print functions related to each get function
+var title = process.argv
+  .splice(3)
+  .join(" ")
+  .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+
+//List of functions for each node command in process.argv[2] that grabs the necessary info from the Twitter, Spotify and OMDB APIs passing callbacks to the print functions related to each get...() function
 var getMyTweets = function() {
   var params = { screen_name: twitter.consumer_key };
   twitter.get("statuses/user_timeline", params, printTweets);
 };
 
 var getSpotifySong = function() {
-  //setting search param : title to a single string by taking each argument after nodeArg and joinng them
   spotify.search({ type: "track", query: title }, printSpotifySong);
 };
 
@@ -28,7 +33,26 @@ var getMovie = function() {
   request(url, printMovie);
 };
 
-var getRandomCommand = function() {};
+var getRandomCommand = function() {
+  fs.readFile("random.txt","UTF-8", function(err, data) {
+    setRandomCommand(data, err);
+    //calling switch case logic function inside of getRandomCommand function in order to execute the command listed in the random.txt file
+    nodeCommandLogic();
+  });
+};
+
+function setRandomCommand(data, err) {
+  var command = data.split(",");
+  if (err)
+    throw err;
+  nodeArg = command[0];
+  if (command.length > 1) {
+    title = command[1].replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+  }
+  else {
+    title = "";
+  }
+}
 
 //List of Functions passed as callbacks in the get functions for each node command in process.argv[2]. These will print the necessary information to the console as well as call the appendLog function once information is received.
 
@@ -39,10 +63,13 @@ function printSpotifySong(err, data) {
   }
   var songInfo =
     "\n" +
+    "Artist: " +
     song.album.artists[0].name +
     "\n" +
+    "Preview URL " +
     song.preview_url +
     "\n" +
+    "Album: "+
     song.album.name +
     "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n";
   console.log(songInfo);
@@ -76,26 +103,26 @@ function printMovie(error, response, body) {
   var movieObject = JSON.parse(body);
   var movieDetails =
     "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n\n" +
-    "Title: "+
+    "Title: " +
     movieObject.Title +
     "\n" +
-    "Year: "+
+    "Year: " +
     movieObject.Year +
     "\n" +
     "imdb Rating: " +
     movieObject.imdbRating +
     "\n" +
-    "Rotten Tomatoes: "+
+    "Rotten Tomatoes: " +
     movieObject.Ratings[1].Value +
-    "\n"+
-    "Country: "+
-    movieObject.Country+
-    "\n"+
-    "Plot: "+
-    movieObject.Plot+
-    "\n"+
-    "Actors: "+
-    movieObject.Actors+
+    "\n" +
+    "Country: " +
+    movieObject.Country +
+    "\n" +
+    "Plot: " +
+    movieObject.Plot +
+    "\n" +
+    "Actors: " +
+    movieObject.Actors +
     "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n";
   console.log(movieDetails);
   appendLog(movieDetails);
@@ -109,17 +136,22 @@ function appendLog(print) {
 }
 
 //This is the switch case that will run a function depending on which argument the user passes as nodeArg(process.argv[2])
-switch (nodeArg) {
-  case "my-tweets":
-    getMyTweets();
-    break;
-  case "spotify-this-song":
-    getSpotifySong();
-    break;
-  case "movie-this":
-    getMovie();
-    break;
-  case "do-what-it-says":
-    doWhatItSays();
-    break;
+
+function nodeCommandLogic() {
+  switch (nodeArg) {
+    case "my-tweets":
+      getMyTweets();
+      break;
+    case "spotify-this-song":
+      getSpotifySong();
+      break;
+    case "movie-this":
+      getMovie();
+      break;
+    case "do-what-it-says":
+      getRandomCommand();
+      break;
+  }
 }
+
+nodeCommandLogic();
